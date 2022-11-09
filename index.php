a16bae360ce5afcf125c5f4ec19e61597a593f46
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-09 20:54:17 +0600

    Initial File

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+
+Plugin Name: Registration Form
+Plugin URL: https://ibrahimkhalil.info/
+Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad unde commodi nam assumenda, neque quasi exercitationem dolores quo, asperiores suscipit, aliquam obcaecati explicabo facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, ut tempore. Qui itaque fuga assumenda!
+Version: 0.1
+Author: Ibrahim Khalil
+Author URI: https://www.facebook.com/mdibrahimk48
+
+*/
+
+?>
