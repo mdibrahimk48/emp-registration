58b6ee4dab8ebacbdc4b9f43123934cbded6bbd9
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-09 21:25:01 +0600

    Enclosing ShortCode

--- index.php
+++ index.php
@@ -14,10 +14,16 @@ Author URI: https://www.facebook.com/mdibrahimk48
 add_shortcode('show', 'ik_address');
 
 if(!function_exists('ik_address')){
-    function ik_address(){
-        $output = "Ibrahim Khalil </br>";
-        $output.="01517816814";
-        return $output;
+
+    function ik_address($atts=[], $content=null){
+
+        // $output = "Ibrahim Khalil </br>";
+        // $output.="01517816814";
+
+        $content = do_shortcode($content);
+
+        return $content;
+
     }
 }
 
