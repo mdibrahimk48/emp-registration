ff24fbbc274bcc43ca7d55d48d9fc7cb4c30ab4c
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-27 23:19:46 +0600

    Class File Include

--- class-emp.php
+++ class-emp.php
@@ -1,5 +1,37 @@
 <?php
 
+class ibrahim_first_plugin{
 
+    function __construct(){
+        add_action('init',array($this,'ibrahim_emp'));
+    }
+
+    function activate(){
+       flush_rewrite_rules(); 
+    }
+    function deactivate(){
+        
+    }
+    function uninstall(){
+        
+    }
+
+
+    function ibrahim_emp(){
+        register_post_type('ibrahim_emp',[
+            'public'    => true,
+            'label'     => "Emp Info"
+        ]);
+    }
+
+
+}
+
+if(class_exists('ibrahim_first_plugin')){
+    $plugin_newInfo = new ibrahim_first_plugin('');
+}
+
+register_activation_hook(__FILE__, array($plugin_newInfo,'activate'));
+register_deactivation_hook(__FILE__, array($plugin_newInfo,'deactivate'));
 
 ?>
--- index.php
+++ index.php
@@ -17,41 +17,6 @@ define(__FILE__,'ABSPATH') or die('You are not human!');
 require_once( EMPLOYEE__PLUGIN_DIR . 'class-emp.php' );
 
 
-class ibrahim_first_plugin{
-
-    function __construct(){
-        add_action('init',array($this,'ibrahim_emp'));
-    }
-
-    function activate(){
-       flush_rewrite_rules(); 
-    }
-    function deactivate(){
-        
-    }
-    function uninstall(){
-        
-    }
-
-
-    function ibrahim_emp(){
-        register_post_type('ibrahim_emp',[
-            'public'    => true,
-            'label'     => "Emp Info"
-        ]);
-    }
-
-
-}
-
-if(class_exists('ibrahim_first_plugin')){
-    $plugin_newInfo = new ibrahim_first_plugin('');
-}
-
-register_activation_hook(__FILE__, array($plugin_newInfo,'activate'));
-register_deactivation_hook(__FILE__, array($plugin_newInfo,'deactivate'));
-
-
 /* Short Code -------------------------------------------------------------------------
     //ShortCode Start
     add_shortcode('show', 'ik_address');
