e80c637769c31ca8050e09a8b8fa05e158c6644b
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-27 19:45:25 +0600

    Plug Update

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 
 /*
 
-Plugin Name         : Registration Form
-Plugin URL          : https://ibrahimkhalil.info/
-Description         : Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad unde commodi nam assumenda, neque quasi exercitationem dolores quo, asperiores suscipit, aliquam obcaecati explicabo facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, ut tempore. Qui itaque fuga assumenda!
-Version             : 0.1
-Author              : Ibrahim Khalil
-Author URI          : https://www.facebook.com/mdibrahimk48
+Plugin Name:        Employee Form
+Plugin URL:         https://ibrahimkhalil.info/
+Description:        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad unde commodi nam assumenda, neque quasi exercitationem dolores quo, asperiores suscipit, aliquam obcaecati explicabo facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, ut tempore. Qui itaque fuga assumenda!
+Version:            0.1
+Author:             Ibrahim Khalil
+Author URI:         https://www.facebook.com/mdibrahimk48
+Text Domain:        ibrahim-plugin
 
 */
 
