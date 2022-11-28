86a6a5cc5b71209eb00624fc607f26bb99cc3db0
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-28 21:43:19 +0600

    Menu Creation

--- class-menu.php
+++ class-menu.php
@@ -1,12 +1,27 @@
 <?php
 
-class menu_admin{
+class menuadmin{
 
     public function menuregister(){
         add_action('admin_menu', array($this, 'mymenu'));
     }
+    public function mymenu(){
+        add_menu_page('admin_m',
+        'Admin Setting',
+        'manage_options',
+        'admin_m',
+        array($this, 'myadminsetting'),
+        'dashicons-admin-tools', 
+       99);
+    }
+
+    public function myadminsetting(){
+        echo "Allah Mohan";
+    }
 }
 
-$menuAdmin = new menuAdmin();
+$menuadmin = new menuadmin();
+
+$menuadmin-> menuregister();
 
 ?>
